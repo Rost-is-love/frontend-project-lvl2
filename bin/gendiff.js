9cae4595cd397631bb66ae4b87d685b883bf93c8
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2020-12-20 14:30:40 +0300

    clean gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,20 +2,6 @@
 
 import program from 'commander';
 
-/* program
-  .name('gendiff')
-  .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.')
-  .option("-h, --help", "output usage information")
-
-program.on('--help', () => {
-  console.log('sdcsdc');
-  console.log('Example call:');
-  console.log('  $ custom-help --help');
-});
-
-program.parse(process.argv); */
-
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
