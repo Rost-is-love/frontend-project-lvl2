aecdbf529f3eb5b87428658b464815f2f3b1ca13
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2020-12-20 18:27:35 +0300

    add new arguments

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import program from 'commander';
 export default () => {
   program
     .version('0.0.1')
+    .arguments('<filepath1> <filepath2>')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'output format');
   program.on('--help', () => {});
