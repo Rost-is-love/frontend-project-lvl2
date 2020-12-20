807a013b4f29cc9da6c525a49c594750fa644d15
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2020-12-20 18:23:31 +0300

    create option format

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import program from 'commander';
 
 export default () => {
-  program.version('0.0.1').description('Compares two configuration files and shows a difference.');
+  program
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format');
   program.on('--help', () => {});
   program.parse(process.argv);
 };
