d3a1655e11f02b0f577862209dbd27075735d581
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2020-12-20 18:20:42 +0300

    create index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 
-import program from 'commander';
+import genDiff from '../src/index.js';
 
-program.version('0.0.1').description('Compares two configuration files and shows a difference.');
-
-program.on('--help', () => {});
-
-program.parse(process.argv);
+genDiff();
