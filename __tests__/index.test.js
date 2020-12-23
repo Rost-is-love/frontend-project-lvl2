c3091f1880dae509ba2849bdf22abf8ad9183596
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2020-12-23 16:01:52 +0300

    create test for flat yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,7 +12,15 @@ const getFixturePath = (filepath) => join(__dirname, '..', '__tests__', '__fixtu
 test('flat json', () => {
   const path1 = getFixturePath('flatjson1.json');
   const path2 = getFixturePath('flatjson2.json');
-  const expected = fs.readFileSync(getFixturePath('expectedjson.txt'), 'utf-8');
+  const expected = fs.readFileSync(getFixturePath('expectedflat.txt'), 'utf-8');
+  expect(findDiff(path1, path2)).toEqual(expected);
+  expect(findDiff(path1, path2)).not.toEqual('');
+});
+
+test('flat yaml', () => {
+  const path1 = getFixturePath('flatyaml1.yml');
+  const path2 = getFixturePath('flatyaml2.yml');
+  const expected = fs.readFileSync(getFixturePath('expectedflat.txt'), 'utf-8');
   expect(findDiff(path1, path2)).toEqual(expected);
   expect(findDiff(path1, path2)).not.toEqual('');
 });
