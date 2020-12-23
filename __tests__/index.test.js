6520389821eca06e8f2f606ba760a1b848b23935
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2020-12-23 15:42:43 +0300

    fix getPath

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,21 +1,18 @@
 /* eslint-disable no-underscore-dangle */
 import { test, expect } from '@jest/globals';
 import fs from 'fs';
-import { dirname, resolve } from 'path';
+import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { findDiff } from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const getFixturePath = (filepath) => resolve(__dirname, filepath);
+const getFixturePath = (filepath) => join(__dirname, '..', '__tests__', '__fixtures__', filepath);
 
 test('flat json', () => {
-  const path1 = getFixturePath('../__tests__/__fixtures__/flatjson1.json');
-  const path2 = getFixturePath('../__tests__/__fixtures__/flatjson2.json');
-  const expected = fs.readFileSync(
-    getFixturePath('../__tests__/__fixtures__/expectedjson.txt'),
-    'utf-8',
-  );
+  const path1 = getFixturePath('flatjson1.json');
+  const path2 = getFixturePath('flatjson2.json');
+  const expected = fs.readFileSync(getFixturePath('expectedjson.txt'), 'utf-8');
   expect(findDiff(path1, path2)).toEqual(expected);
   expect(findDiff(path1, path2)).not.toEqual('');
 });
