12dc6831430041426d0e93b67d1e880a6bf0b01e
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2020-12-31 19:51:27 +0300

    create differ

---
+++ src/differ.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+
+const getDiff = (data1, data2) => {
+  const keys = _.union(_.keys(data1), _.keys(data2)).sort();
+
+  return keys.map((key) => {
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return { key, status: 'nested', children: getCurData(data1[key], data2[key]) };
+    }
+    if (data1[key] === data2[key]) {
+      return { key, status: 'outdated', value: data1[key] };
+    }
+    if (_.has(data1, key) && _.has(data2, key)) {
+      return { key, status: 'updated', curValue: data2[key], oldValue: data1[key] };
+    }
+
+    return _.has(data1, key)
+      ? { key, status: 'removed', value: data1[key] }
+      : { key, status: 'added', value: data2[key] };
+  });
+};
+
+export default getDiff();
