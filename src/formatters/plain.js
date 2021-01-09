f47cc7042deba50c51aabfe1603d5fa8788ef410
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2021-01-09 14:37:30 +0300

    add getFunc

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,47 +1,29 @@
 import _ from 'lodash';
 
-const getValue = (value) => (value === null || typeof value === 'boolean' ? value : `'${value}'`);
+const getValue = (value) =>
+  _.isPlainObject(value)
+    ? '[complex value]'
+    : value === null || typeof value === 'boolean'
+    ? value
+    : `'${value}'`;
 
-const format = (data1, data2, path = '') => {
-  const keys = _.union(_.keys(data1), _.keys(data2)).sort();
+const getFunc = {
+  outdated: () => '',
+  updated: (path, obj) =>
+    `Property '${path}' was updated. From ${getValue(obj.oldValue)} to ${getValue(obj.curValue)}`,
+  removed: (path) => `Property '${path}' was removed`,
+  added: (path, obj) => `Property '${path}' was added with value: ${getValue(obj.value)}`,
+  nested: (path, obj) => {
+    const filterChildren = obj.children.filter((child) => child.status !== 'outdated');
+    return [
+      ...filterChildren.flatMap((obj) => getFunc[obj.status](`${path}.${obj.key}`, obj)),
+    ].join('\n');
+  },
+};
 
-  return keys
-    .reduce((acc, key) => {
-      if (!_.has(data1, key)) {
-        if (_.isObject(data2[key])) {
-          return [...acc, `Property '${path}${key}' was added with value: [complex value]`];
-        }
-        return [...acc, `Property '${path}${key}' was added with value: ${getValue(data2[key])}`];
-      }
-      if (!_.has(data2, key)) {
-        return [...acc, `Property '${path}${key}' was removed`];
-      }
-      if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-        return [...acc, format(data1[key], data2[key], `${path}${key}.`)];
-      }
-      if (data1[key] !== data2[key]) {
-        if (_.isObject(data1[key]) && !_.isObject(data2[key])) {
-          return [
-            ...acc,
-            `Property '${path}${key}' was updated. From [complex value] to ${getValue(data2[key])}`,
-          ];
-        }
-        if (!_.isObject(data1[key]) && _.isObject(data2[key])) {
-          return [
-            ...acc,
-            `Property '${path}${key}' was updated. From '${data1[key]}' to [complex value]`,
-          ];
-        }
-        return [
-          ...acc,
-          `Property '${path}${key}' was updated. From ${getValue(data1[key])} to ${getValue(
-            data2[key],
-          )}`,
-        ];
-      }
-      return acc;
-    }, [])
-    .join('\n');
+const format = (data) => {
+  const filterChildren = data.filter((obj) => obj.status !== 'outdated');
+  return [...filterChildren.flatMap((obj) => getFunc[obj.status](obj.key, obj))].join('\n');
 };
 
 export default format;
