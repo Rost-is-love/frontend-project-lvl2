433837ab43a3183e28d97d2f44796260df63e0b6
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2021-01-10 11:48:35 +0300

    edit test's file names

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import fs from 'fs';
-import { test, expect } from '@jest/globals';
+import { describe, test, expect } from '@jest/globals';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../index.js';
@@ -8,10 +8,10 @@ import genDiff from '../index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filepath) => join(__dirname, '..', '__fixtures__', filepath);
-const jsonPath1 = getFixturePath('flatjson1.json');
-const jsonPath2 = getFixturePath('flatjson2.json');
-const yamlPath1 = getFixturePath('flatyaml1.yml');
-const yamlPath2 = getFixturePath('flatyaml2.yml');
+const jsonPath1 = getFixturePath('json1.json');
+const jsonPath2 = getFixturePath('json2.json');
+const yamlPath1 = getFixturePath('yaml1.yml');
+const yamlPath2 = getFixturePath('yaml2.yml');
 const expectedStylish = fs.readFileSync(getFixturePath('expected-stylish.txt'), 'utf-8');
 const expectedPlain = fs.readFileSync(getFixturePath('expected-plain.txt'), 'utf-8');
 const expectedJson = fs.readFileSync(getFixturePath('expected-json.txt'), 'utf-8');
