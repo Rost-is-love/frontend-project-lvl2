13cf8410b96fd62fc1f5f290a01ff47c892d7d72
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2021-01-23 11:24:31 +0300

    swap checks from easy to hard

--- src/differ.js
+++ src/differ.js
@@ -4,21 +4,21 @@ const getDiff = (data1, data2) => {
   const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
 
   return keys.map((key) => {
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return { key, status: 'nested', children: getDiff(data1[key], data2[key]) };
+    if (!_.has(data1, key)) {
+      return { key, status: 'added', value: data2[key] };
+    }
+    if (!_.has(data2, key)) {
+      return { key, status: 'removed', value: data1[key] };
     }
     if (_.isEqual(data1[key], data2[key])) {
       return { key, status: 'unchanged', value: data1[key] };
     }
-    if (_.has(data1, key) && _.has(data2, key)) {
-      return {
-        key, status: 'updated', newValue: data2[key], oldValue: data1[key],
-      };
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return { key, status: 'nested', children: getDiff(data1[key], data2[key]) };
     }
-
-    return _.has(data1, key)
-      ? { key, status: 'removed', value: data1[key] }
-      : { key, status: 'added', value: data2[key] };
+    return {
+      key, status: 'updated', newValue: data2[key], oldValue: data1[key],
+    };
   });
 };
 
