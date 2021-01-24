3ca8831d86c3bf172eeb8263ff9162e02a76d652
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2021-01-24 18:21:41 +0300

    remove case ''

--- src/parsers.js
+++ src/parsers.js
@@ -3,7 +3,6 @@ import yaml from 'js-yaml';
 export default (data, dataType) => {
   switch (dataType) {
     case 'json':
-    case '':
       return JSON.parse(data);
     case 'yml':
     case 'yaml':
